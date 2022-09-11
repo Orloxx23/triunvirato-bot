23184f38dc5f149ceddee3ad8708bc2d28c55ce2
Orlando Mina <65779635+Orloxx23@users.noreply.github.com>
2022-09-11 03:01:16 -0500

    bot presence edited

--- events/ready.js
+++ events/ready.js
@@ -5,5 +5,6 @@ module.exports = {
 		client.user.setPresence({ activities: [{ name: '/ayuda | Para servirte', type: 'Watching' }], status: 'online' });
 
 		console.log(`\nReady! Logged in as ${client.user.tag}`.white.bold);
+		console.log(`${client.user.presence.activities[0].name}`.white.bold);
 	},
 };
