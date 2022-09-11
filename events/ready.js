4f786ee6c75078f6669b7a1bc2dea585df919ed6
Orlando Mina <65779635+Orloxx23@users.noreply.github.com>
2022-09-11 02:43:27 -0500

    Set presence on ready event

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,13 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
+		client.user.setPresence({
+			activity: {
+				name: '/ayuda | Para servirte',
+				type: 'WATCHING',
+			},
+			status: 'online',
+		});
 		console.log(`\nReady! Logged in as ${client.user.tag}`.white.bold);
 	},
 };
