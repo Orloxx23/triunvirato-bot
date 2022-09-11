adaeceb93c5f7e189b6cc6e9e0539eec49af5c7a
Orlando Mina <65779635+Orloxx23@users.noreply.github.com>
2022-09-11 00:38:18 -0500

    Deleted some commands and meme command added

---
+++ commands/meme.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+const red = require('reddit-fetch');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('meme')
+		.setDescription('Obten un meme aleatorio.'),
+	async execute(interaction) {
+		red({
+			subreddit: 'SpanishMeme',
+			sort: 'hot',
+			allowNSFW: false,
+			allowModPost: false,
+			allowCrossPost: false,
+			allowVideo: true,
+		}).then(async (post) => {
+			if (!post.url) {
+				return await interaction.reply('No encontre memes :(');
+			}
+			else {
+				const exampleEmbed = {
+					color: 0xFFFFFF,
+					title: post.title,
+					image: {
+						url: post.url,
+					},
+				};
+				await interaction.reply({ embeds: [exampleEmbed] });
+			}
+		});
+		// await do something;
+	},
+};
