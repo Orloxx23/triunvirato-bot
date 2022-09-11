2be5af05c80d41921ba7967a337988fd5d421cf4
Orlando Mina <65779635+Orloxx23@users.noreply.github.com>
2022-09-11 13:54:12 -0500

    Show bot activity

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,10 @@
+const { ActivityType } = require('discord.js');
+
 module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-		client.user.setPresence({ activities: [{ name: '/ayuda | Para servirte', type: 'Watching' }], status: 'online' });
+		client.user.setActivity('/ayuda | Para servirte', { type: ActivityType.Watching });
 
 		console.log(`\nReady! Logged in as ${client.user.tag}`.white.bold);
 		console.log(`${client.user.presence.activities[0].name}`.white.bold);
